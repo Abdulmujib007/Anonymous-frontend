edd0d88369134fd0efb0b40862ad26860c44c70c
Abdulmujib <asiyanbiakorede@gmail.com>
2021-09-11 20:08:27 +0100

    sigh up added

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import Fetch from "../helper/Fetch";
-import { Link } from "react-router-dom";
 
 const Messages = ({ history }) => {
   const [message, setMessage] = useState([]);
